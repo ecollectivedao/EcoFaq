9c61ff0b9578c5736250b8d5a0fa14f5c381389f
Artem Lisovik <107492333+ArtemLisovik@users.noreply.github.com>
2023-04-26 20:39:07 +0300

    main animation

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,24 +1,22 @@
-import {Header} from 'containers'
-import { News } from 'containers/News/News'
-import { News2 } from 'containers/News_2/News_2'
-import {Promo} from 'containers/Promo/Promo'
-import {NewsCenter} from 'containers/NewsCenter/NewsCenter'
-import {Footer} from 'containers/Footer/Footer'
+import { Header } from 'containers'
+import { Footer } from 'containers/Footer/Footer'
 
-import image from 'assets/img/4.png'
-import { Investors } from 'containers/Investors/Investors'
+export const MainLayout = ({ children }: { children: React.ReactNode }) => {
 
-export const MainLayout = ({children}: {children: React.ReactNode}) => {
     return (
         <div className='wrapper__gradient'>
-            <Header/>
-            
+            <Header />
+            <div id='stars'></div>
+            <div id='stars2'></div>
+            <div id='stars3'></div>
+
             <main className="main">
+
                 <div className="container__body">
-                  {children}
+                    {children}
                 </div>
             </main>
-            <Footer/>
+            <Footer />
         </div>
     )
 }
