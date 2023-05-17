bb3b5171f6563c6fc45909dcaacbbf072148539e
Artem Lisovik <107492333+ArtemLisovik@users.noreply.github.com>
2023-05-17 15:24:51 +0300

    updated info v2

--- src/containers/Promo/Promo.tsx
+++ src/containers/Promo/Promo.tsx
@@ -5,10 +5,10 @@ import './Promo.scss'
 export const Promo = () => {
     return (
         <section className='promo'>
-            <h1 className="promo__title">
+            {/* <h1 className="promo__title">
                 Eco - это не банк
-            </h1>
-            <h2 className="promo__subtitle">
+            </h1> */}
+            <h1 className="promo__subtitle">
                 ЕСОсommunity - это сообщество людей с единой целью  
                 
                 <span className='promo__typing'>
@@ -23,7 +23,7 @@ export const Promo = () => {
                 />
                 </span>
                 для ЕСОпротокола
-            </h2>
+            </h1>
             <p className="promo__descr">
                 На данном сайте вы узнаете всю основную информацию по проекту ЕСО, актуальные задания, перевод важных документов и т.д <br />
                 Сайт находится на ранней стадии
